cdf9ca00bef0d32ece00cbae4ba1fa5ca8dd907a
iradu <ionut.radu@tremend.com>
2019-05-10 16:03:05 +0300

    upload to FTP SERVER

--- bin/ftp-upload.sh
+++ bin/ftp-upload.sh
@@ -1,18 +1,29 @@
 #!/bin/bash
 
 
-if [[ -z $FTP_HOST ]] || [[ -z $FTP_USER ]] || [[ -z $FTP_PASS ]] || [[ -z $FTP_REMOTE_FOLDER ]] || [[ -z $LOG_FILENAME ]]; then
+if [[ -z $FTP_HOST ]] || [[ -z $FTP_USER ]] || [[ -z $FTP_PASS ]] || [[ -z $FTP_REMOTE_FOLDER ]] || [[ -z $LOG_FILENAME ]] || [[ -z $FTP_PORT ]]; then
     echo "Please provide FTP_HOST, FTP_USER, FTP_PASS, FTP_REMOTE_FOLDER, LOG_FILENAME"
     exit (1)
 fi
 
-LOG_TMP_FILE="/var/log/tmp/access.log-(date +%Y-%m-%d).gz"
+LOG_TMP_FILE="/var/log/tmp/access.log-$(date +%Y-%m-%d-%T).gz"
 
 if [[ -n GREP_EXCLUDE_WORD ]];
    then
-      grep -i -v $GREP_EXCLUDE_WORD $LOG_FILENAME | gzip > $LOG_TMP_FILE
+      egrep -i -v $GREP_EXCLUDE_WORD $LOG_FILENAME | gzip > $LOG_TMP_FILE
    else
        mv  $LOG_FILENAME  $LOG_TMP_FILE
 
 fi
+
+# upload to FTP server
+
+for FILE_UPLOAD in $(ls /var/log/tmp/*.gz
+do
+    curl -s -T $FILE_UPLOAD "ftp://${FTP_HOST}:${FTP_PORT}/${FTP_REMOTE_FOLDER}/" --user $FTP_USER:$FTP_PASS
+    if [ $? -eq 0 ]; then
+       echo "$FILE_UPLOAD sent to FTP server $FTP_HOST"
+       rm $FILE_UPLOAD
+    fi
+done
 
