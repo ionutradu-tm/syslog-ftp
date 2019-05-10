89d0a4b9b969061fe330e642a0e930ef35e160cd
iradu <ionut.radu@tremend.com>
2019-05-10 15:15:25 +0300

    run logrotate hourly

--- bin/run.sh
+++ bin/run.sh
@@ -4,7 +4,7 @@ mkdir -p /var/log/tmp
 mkdir -p /var/log/remote
 
 #run logrotate hourly
-mv /etc/crond.daily/logrotate /etc/cron.hourly/
+mv /etc/cron.daily/logrotate /etc/cron.hourly/
 
 # start cron service
 service cron start
