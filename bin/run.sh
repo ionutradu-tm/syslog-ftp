be413982e420f5aaefd55d0b7fdded41ac0b703c
iradu <ionut.radu@tremend.com>
2019-05-13 15:44:42 +0300

    export env to cron

--- bin/run.sh
+++ bin/run.sh
@@ -17,14 +17,14 @@ if [[ -z $FTP_HOST ]] || [[ -z $FTP_USER ]] || [[ -z $FTP_PASS ]] || [[ -z $FTP_
 fi
 # prepare env variables
 echo "#!/bin/bash" > /usr/local/bin/env.sh
-echo "export FTP_USER=$FTP_USER" >>/usr/local/bin/env.sh
-echo "export FTP_PASS=$FTP_PASS" >>/usr/local/bin/env.sh
-echo "export FTP_HOST=$FTP_HOST" >>/usr/local/bin/env.sh
-echo "export FTP_PORT=$FTP_PORT" >>/usr/local/bin/env.sh
-echo "export FTP_REMOTE_FOLDER=$FTP_REMOTE_FOLDER" >>/usr/local/bin/env.sh
-echo "export LOG_FILENAME=$LOG_FILENAME" >>/usr/local/bin/env.sh
+echo "export FTP_USER=\"$FTP_USER\"" >>/usr/local/bin/env.sh
+echo "export FTP_PASS=\"$FTP_PASS\"" >>/usr/local/bin/env.sh
+echo "export FTP_HOST=\"$FTP_HOST\"" >>/usr/local/bin/env.sh
+echo "export FTP_PORT=\"$FTP_PORT\"" >>/usr/local/bin/env.sh
+echo "export FTP_REMOTE_FOLDER=\"$FTP_REMOTE_FOLDER\"" >>/usr/local/bin/env.sh
+echo "export LOG_FILENAME=\"$LOG_FILENAME\"" >>/usr/local/bin/env.sh
 if [[ -n $GREP_EXCLUDE_WORD ]]; then
-   echo "export GREP_EXCLUDE_WORD=$GREP_EXCLUDE_WORD" >>/usr/local/bin/env.sh
+   echo "export GREP_EXCLUDE_WORD=\"$GREP_EXCLUDE_WORD\"" >>/usr/local/bin/env.sh
 fi
 
 chmod +x /usr/local/bin/env.sh
